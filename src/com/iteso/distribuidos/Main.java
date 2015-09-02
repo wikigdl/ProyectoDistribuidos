f3aa7c1f82ce2352b08303fe1c9a2509341a451a
Sergio <wikigdl@gmail.com>
2015-09-02 08:46:54 -0500

    Initial

---
+++ src/com/iteso/distribuidos/Main.java
@@ -0,0 +1,10 @@
+package com.iteso.distribuidos;
+
+/**
+ * Created by Sheko on 02/09/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

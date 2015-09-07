81182bb44a10b2f168d77f3f983520a12707932e
Sergio <wikigdl@gmail.com>
2015-09-06 21:33:45 -0500

    gui added

--- src/com/iteso/distribuidos/Main.java
+++ src/com/iteso/distribuidos/Main.java
@@ -1,10 +1,13 @@
 package com.iteso.distribuidos;
 
+import com.iteso.distribuidos.gui.CityControl;
+
 /**
  * Created by Sheko on 02/09/2015.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        CityControl cityControl= new CityControl();
     }
 }
